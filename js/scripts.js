9cbee64802dedeb07a02c96c4474a246c43fdccd
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-11-25 15:54:32 -0300

    style: add responsivity

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,20 @@ const swiper = new Swiper(".swiper-videos", {
     nextEl: ".last-videos .btn-next",
     prevEl: ".last-videos .btn-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    767: {
+      slidesPerView: 2.1,
+    },
+    1024: {
+      slidesPerView: 3.1,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
 });
